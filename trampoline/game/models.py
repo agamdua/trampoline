90701c72461ef758429ad3261b38a93551bc4d5f
Agam Dua <agamdua@gmail.com>
2014-04-28 12:06:17 -0400

    Added: Initial game models

--- trampoline/game/models.py
+++ trampoline/game/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from user import User
 
-# Create your models here.
+
+PLAYERS = (
+    ('ai', 'Computer'),
+    ('us', 'User'),
+)
+
+MOVES = (
+    ('X', 'X'),
+    ('O', 'O'),
+)
+
+class Game(models.Model):
+    player = models.CharField(max_length = 2,
+                              choices = PLAYERS)
+    player_move = models.CharField(max_length = 1,
+                                   choices = MOVES)
+    winner = models.CharField(default=None,
+                              choices = PLAYERS)
